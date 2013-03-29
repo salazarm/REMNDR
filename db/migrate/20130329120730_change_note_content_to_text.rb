e6a74d522c8eec1bbab3fca26ca733e22c1c2a1b
Marco Salazar <salazarm@mit.edu>
2013-03-29 08:08:59 -0400

    init

---
+++ db/migrate/20130329120730_change_note_content_to_text.rb
@@ -0,0 +1,9 @@
+class ChangeNoteContentToText < ActiveRecord::Migration
+  def up
+  	change_column :notes, :content, :text
+  end
+
+  def down
+  	change_column :notes, :content, :string
+  end
+end
